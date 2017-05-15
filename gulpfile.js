e796ca3b51b78589831c80132fe07bff2abb7d0c
10xl <urnotuurmi@gmail.com>
2017-05-15 03:15:01 -0400

    uglify, minify, gulp task

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+	minifycss = require('gulp-minify-css'),
+	gutil = require('gulp-util'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	usemin = require('gulp-usemin'),
+	ngannotate = require('gulp-ng-annotate'),
+	del = require('del'),
+	rev = require('gulp-rev');
+
+var basePath = '/client';
+// use gulp to uglify, concat and copy to 'dist' folder
+
+gulp.task('usemin', function() {
+	return gulp.src('./client/**/*.html')
+		.pipe(usemin({
+			css: [minifycss(), rev()],
+			js: [ngannotate(), uglify(), rev()]
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function() {
+	return del(['dist']);
+});
+
+gulp.task('default', ['usemin']);
